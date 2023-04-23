ffe2c5127aa005b71898f810f80b313d4e329e8f
Yair Cohen <yair.cohen.mail@gmail.com>
2023-04-24 00:11:21 +0300

    set tab to 4 spaces

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,28 +6,28 @@ import * as fs from "fs";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {
-  }
+    constructor(private readonly appService: AppService) {
+    }
 
-  @Get()
-  home() {
-    return "Palette API Home";
-  }
+    @Get()
+    home() {
+        return "Palette API Home";
+    }
 
-  @Post("url")
-  async imageByLink(@Body("image") image, @Body("size") paletteSize, @Body("variance") variance): Promise<any> {
-    if (!image) throw new HttpException("No Image Url Passed", 400);
-    return await this.appService.main(image, paletteSize, variance);
-  }
+    @Post("url")
+    async imageByLink(@Body("image") image, @Body("size") paletteSize, @Body("variance") variance): Promise<any> {
+        if (!image) throw new HttpException("No Image Url Passed", 400);
+        return await this.appService.main(image, paletteSize, variance);
+    }
 
-  @Post("file")
-  @UseInterceptors(FileInterceptor("image", {
-    dest: "./images"
-  }))
-  async imageByFile(@UploadedFile() image: Express.Multer.File, @Body("size") paletteSize, @Body("variance") variance) {
-    if (!image?.path) throw new HttpException("No Image File Passed", 400);
-    const palettes = await this.appService.main(image.path, paletteSize, variance);
-    await fs.unlinkSync(image.path);
-    return palettes;
-  }
+    @Post("file")
+    @UseInterceptors(FileInterceptor("image", {
+        dest: "./images"
+    }))
+    async imageByFile(@UploadedFile() image: Express.Multer.File, @Body("size") paletteSize, @Body("variance") variance) {
+        if (!image?.path) throw new HttpException("No Image File Passed", 400);
+        const palettes = await this.appService.main(image.path, paletteSize, variance);
+        await fs.unlinkSync(image.path);
+        return palettes;
+    }
 }
