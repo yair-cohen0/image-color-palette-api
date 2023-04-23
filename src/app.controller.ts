820fdfbd959faf434963f5c31f3db48801420644
Yair Cohen <yair.cohen.mail@gmail.com>
2023-04-24 00:07:20 +0300

    added file check with exception & change link to url

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
+import { Body, Controller, Get, HttpException, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
 import { AppService } from "./app.service";
 import { Express } from "express";
 import { FileInterceptor } from "@nestjs/platform-express";
@@ -6,24 +6,26 @@ import * as fs from "fs";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+  }
 
   @Get()
   home() {
     return "Palette API Home";
   }
 
-  @Post('link')
-  async imageByLink(@Body('image') image, @Body('size') paletteSize, @Body('variance') variance): Promise<any> {
+  @Post("url")
+  async imageByLink(@Body("image") image, @Body("size") paletteSize, @Body("variance") variance): Promise<any> {
+    if (!image) throw new HttpException("No Image Url Passed", 400);
     return await this.appService.main(image, paletteSize, variance);
   }
 
-  @Post('file')
-  @UseInterceptors(FileInterceptor('image', {
+  @Post("file")
+  @UseInterceptors(FileInterceptor("image", {
     dest: "./images"
   }))
-  async imageByFile(@UploadedFile() image: Express.Multer.File, @Body('size') paletteSize, @Body('variance') variance) {
-    if (!image?.path) return "No image Passed";
+  async imageByFile(@UploadedFile() image: Express.Multer.File, @Body("size") paletteSize, @Body("variance") variance) {
+    if (!image?.path) throw new HttpException("No Image File Passed", 400);
     const palettes = await this.appService.main(image.path, paletteSize, variance);
     await fs.unlinkSync(image.path);
     return palettes;
